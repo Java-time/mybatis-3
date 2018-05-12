fb0c1d1a6e128257a973d5743e06c2ed67037fe6
kezhenxu94 <kezhenxu94@163.com>
2018-05-12 11:40:41 +0800

    Add documentation RowBounds

--- src/main/java/org/apache/ibatis/session/RowBounds.java
+++ src/main/java/org/apache/ibatis/session/RowBounds.java
@@ -15,7 +15,26 @@
  */
 package org.apache.ibatis.session;
 
+import java.sql.ResultSet;
+
 /**
+ * 返回结果行限制;
+ *
+ * 可以通过在 Mapper 函数的参数中添加 {@link RowBounds} 参数来指定返回偏移量为 {@link RowBounds#offset},
+ * 只返回 {@link RowBounds#limit} 条记录数; 该类一般用于分页;
+ *
+ * 但需要注意的是, 使用 {@link RowBounds} 的分页并不是从数据库层进行限制的, 也就是说 MyBatis 会把数据库的所有数据
+ * 都查出来, 然后根据 {@link RowBounds} 只取其中的一部分, 这在查询返回非常多数据的时候是很慢的;
+ *
+ * 根据 MyBatis (Core) 的设计理念, MyBatis 不会生成依赖特定数据库的 SQL 语句, 因此使用 MyBatis (Core) 进行本地化
+ * 的分页是不可能的, 但可以使用一些 Plugin 实现, 详见: https://github.com/mybatis/mybatis-3/pull/861
+ *
+ * 详见:
+ *
+ * - {@link org.apache.ibatis.executor.resultset.DefaultResultSetHandler#shouldProcessMoreRows(ResultContext, RowBounds)}
+ *
+ * - {@link org.apache.ibatis.executor.resultset.DefaultResultSetHandler#skipRows(ResultSet, RowBounds)}
+ *
  * @author Clinton Begin
  */
 public class RowBounds {
